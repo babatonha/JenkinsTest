d57cec965a14e2ed804e42a240f8006a703aa369
babatonha <tonhaview@gmail.com>
2024-02-19 10:53:57 +0200

    Updated code

--- JenkinsCicdTest.API/Controllers/WeatherForecastController.cs
+++ JenkinsCicdTest.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace JenkinsCicdTest.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",  "Burning"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
